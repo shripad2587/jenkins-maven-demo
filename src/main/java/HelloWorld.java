3b96d31cdf9ce0c8d047ac98ca9127facf173773
shripad.d007 <shripad.d007@gmail.com>
2026-06-11 17:28:20 +0530

    changed the title

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins Maven Build AT HEFSHINE INSTITUTE PUNE");
+        System.out.println("Today is Thursday we are working on Jenkins Platform");
     }
 }
