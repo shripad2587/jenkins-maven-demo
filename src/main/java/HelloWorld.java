257b8e3820c13e0921cbf8e533e3f15f7431a38e
shripad.d007 <shripad.d007@gmail.com>
2026-06-11 16:59:47 +0530

    second push

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins Maven Build");
+        System.out.println("Hello Jenkins Maven Build AT HEFSHINE INSTITUTE PUNE");
     }
 }
